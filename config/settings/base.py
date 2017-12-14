a3fd54d2442c823de265b9933a22b8f9dc57dd50
ihyecheon <ihyecheon@gmail.com>
2017-12-14 23:33:30 +0900

    #1-32 Creating the URLS and testing the Image Serializers

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,10 +45,10 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',  # Form layouts
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'rest_framework',  # REST framework
 ]
 
 # Apps specific for this project go here.
--- python_api/users/admin.py
+++ python_api/users/admin.py
@@ -11,7 +11,6 @@ class MyUserChangeForm(UserChangeForm):
 
 
 class MyUserCreationForm(UserCreationForm):
-
     error_message = UserCreationForm.error_messages.update({
         'duplicate_username': 'This username has already been taken.'
     })
@@ -33,7 +32,7 @@ class MyUserAdmin(AuthUserAdmin):
     form = MyUserChangeForm
     add_form = MyUserCreationForm
     fieldsets = (
-            ('User Profile', {'fields': ('name',)}),
-    ) + AuthUserAdmin.fieldsets
+                    ('User Profile', {'fields': ('name', 'followers', 'following',)}),
+                ) + AuthUserAdmin.fieldsets
     list_display = ('username', 'name', 'is_superuser')
     search_fields = ['name']
