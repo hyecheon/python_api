3d5c5174f0c362724a5a2c97d539802e3a52a063
ihyecheon <ihyecheon@gmail.com>
2018-01-02 23:30:42 +0900

    #3-16 Moving the App Component and Adding Reset-CSS

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import styles from 'App.scss';
+import styles from './styles.scss';
 
 class App extends Component {
     render() {
         return (
-            <div className={styles.App}/>
+            <div className={styles}/>
         );
     }
 }
