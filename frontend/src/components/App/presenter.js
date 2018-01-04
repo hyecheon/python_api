b785bace268c8334ad23a154fab3eab3db7a2a39
ihyecheon <ihyecheon@gmail.com>
2018-01-05 04:38:50 +0900

    #3-24 Auth Component part One

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Route, Switch} from 'react-router-dom';
-import styles from './styles.scss';
 import Footer from 'components/Footer'
+import Auth from "components/Auth/presenter";
 
 
 const App = props => [
     props.isLoggedIn ? <PrivateRoutes key={2}/> : <PublicRoutes key={2}/>,
     < Footer key={3}/>
 ];
+
+App.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+};
 
 const PrivateRoutes = props => (
     <Switch>
@@ -18,7 +23,7 @@ const PrivateRoutes = props => (
 
 const PublicRoutes = props => (
     <Switch>
-        <Route exact path={"/"} render={() => 'login!'}/>
+        <Route exact path={"/"} render={Auth}/>
         <Route exact path={"/forgot"} render={() => 'password'}/>
     </Switch>
 );
