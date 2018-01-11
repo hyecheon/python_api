bdaf9e839880f3e4b60f86efc75fd8ada0574300
ihyecheon <ihyecheon@gmail.com>
2018-01-11 22:47:33 +0900

    #3-43 Dealing with Blocked Updates

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -2,10 +2,11 @@ import {connect} from 'react-redux';
 import Container from "./container";
 
 const mapStateToProps = (state, ownProps) => {
-    const {user} = state;
-    return {
-        isLoggedIn: user.isLoggedIn
-    };
+	const {user, routing: {location}} = state;
+	return {
+		isLoggedIn: user.isLoggedIn,
+		pathname: location.pathname
+	};
 };
 
 export default connect(mapStateToProps)(Container)
