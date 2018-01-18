6e86f919c3d3d7aa23d6904377a1470eb0b7c543
ihyecheon <ihyecheon@gmail.com>
2018-01-18 23:00:54 +0900

    #3-50 FeedPhoto Component pt. Two

---
+++ frontend/src/components/PhotoActions/container.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import PhotoActions from './presenter'
+
+class Container extends Component {
+	render() {
+		return (
+			<PhotoActions {...this.props}/>
+		);
+	}
+}
+
+export default Container;
